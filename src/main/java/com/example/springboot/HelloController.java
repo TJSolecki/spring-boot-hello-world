6b4ff27dd535f885eff285e9b34b348501606ec5
Thomas Solecki <thomasjsolecki@gmail.com>
2024-05-18 09:27:28 -0700

    Add echo endpoint

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @PostMapping("/echo")
+    public String echo(@RequestParam(value = "message") String message) {
+        return message;
     }
 }
